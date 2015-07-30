3de7b6be8c1b499cdafcb7787c720ec60a4e55f0
lukasz.harasimowicz <developer@hardev.pl>
2015-07-30 23:50:18 +0200

    Project restructurization and proper file encoding.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
+ # -*- coding: utf-8 -*-
+
 from distutils.core import setup
 
 setup(
-    name='deployer_lite_core',
+    name='deployer_lite_agent',
     version='0.0.1',
-    packages=[''],
+    packages=['agent', 'core', 'server', ],
     url='https://github.com/harnash/deployer-lite',
     license='Apache 2',
-    author='£ukasz Harasimowicz',
+    author='≈Åukasz Harasimowicz',
     author_email='dev@harnash.eu',
     description='',
     tests_require=[
         'nose==1.3.7',
     ],
+    install_requires=[
+        'pyzmq==14.7.0',
+        'click==4.1',
+    ]
 )
