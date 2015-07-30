5b2932d31fad6c4464a4ed2f81e90e956af3deaf
lukasz.harasimowicz <developer@hardev.pl>
2015-07-30 23:34:57 +0200

    Added initial setup.py and updated .gitignore

---
+++ core/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='deployer_lite_core',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/harnash/deployer-lite',
+    license='Apache 2',
+    author='£ukasz Harasimowicz',
+    author_email='dev@harnash.eu',
+    description='',
+    tests_require=[
+        'nose==1.3.7',
+    ],
+)
---
+++ server/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='deployer_lite_server',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/harnash/deployer-lite',
+    license='Apache 2',
+    author='£ukasz Harasimowicz',
+    author_email='dev@harnash.eu',
+    description='',
+    install_requires=[
+        'pyzmq==14.7.0',
+    ]
+)
