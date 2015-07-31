ca76f2abe061032041cd51896a6e14a6bc6fe700
Łukasz Harasimowicz <lukasz.harasimowicz@wikia-inc.com>
2015-07-31 15:50:10 +0200

    Updated package information and Travis-CI, coveralls.io integration.

---
+++ server/commands/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import click
+
+
+@click.command()
+def register():
+    print("Works!\n")
--- setup.py
+++ setup.py
@@ -1,18 +1,51 @@
 # -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
 
-from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), 'rb') as f:
+    long_description = f.read().decode('utf-8')
 
 setup(
     name='deployer_lite',
     version='0.0.1',
-    packages=['agent', 'core', 'server', ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     url='https://github.com/harnash/deployer-lite',
-    license='Apache 2',
+    license='Apache 2.0',
     author='Łukasz Harasimowicz',
     author_email='dev@harnash.eu',
     description='',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: Software Distribution',
+        'Topic :: System :: Systems Administration',
+
+        'License :: OSI Approved :: :: Apache Software License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    entry_points={
+        'console_scripts': [
+            'deployer-server = server.commands:register',
+        ],
+    },
     tests_require=[
         'nose==1.3.7',
+        'coverage==4.0a6',
     ],
     install_requires=[
         'pyzmq==14.7.0',
