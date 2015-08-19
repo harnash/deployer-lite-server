8f80f13183d1e7ad5a6e273a788f34397f5f91cd
lukasz.harasimowicz <developer@hardev.pl>
2015-08-19 23:16:17 +0200

    Fixed server command to properly listen and accept connections.

--- server/commands/__init__.py
+++ server/commands/__init__.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 import click
-import time
 import zmq
 from pep3143daemon import DaemonContext, PidFile
 
@@ -21,48 +20,43 @@ def register():
 @click.option('-d', '--dry-run/--no-dry-run', default=False)
 def run(daemon, dry_run):
     def main():
-        click.echo('Daemon starting...')
         context = zmq.Context()
 
-        # First, connect our subscriber socket
-        subscriber = context.socket(zmq.SUB)
-        subscriber.connect('tcp://localhost:5561')
-        subscriber.setsockopt(zmq.SUBSCRIBE, b'')
+        # Socket to talk to clients
+        publisher = context.socket(zmq.PUB)
+        # set SNDHWM, so we don't drop messages for slow subscribers
+        publisher.sndhwm = 1100000
+        publisher.bind('tcp://*:5561')
 
-        time.sleep(1)
-
-        # Second, synchronize with publisher
-        syncclient = context.socket(zmq.REQ)
-        syncclient.connect('tcp://localhost:5562')
+        # Socket to receive signals
+        syncservice = context.socket(zmq.REP)
+        syncservice.bind('tcp://*:5562')
 
         if dry_run:
             return 0
 
-        click.echo('Synchronizing')
+        # Get synchronization from subscribers
+        subscribers = 0
+        while subscribers < 3:
+            # wait for synchronization request
+            msg = syncservice.recv()
+            # send synchronization reply
+            syncservice.send(b'')
+            subscribers += 1
+            click.echo("+1 subscriber (%i/%i)" % (subscribers, 3))
 
-        # send a synchronization request
-        syncclient.send(b'')
+        # Now broadcast exactly 1M updates followed by END
+        for i in range(1000000):
+            publisher.send(b'Rhubarb')
 
-        # wait for synchronization reply
-        syncclient.recv()
-
-        click.echo('Synchronizing - done')
-
-        # Third, get our updates and report how many we got
-        nbr = 0
-        while True:
-            msg = subscriber.recv()
-            if msg == b'END':
-                break
-            nbr += 1
-
-        click.echo('Received %d updates' % nbr)
+        publisher.send(b'END')
 
         return 0
 
     if daemon:
         pid_file = PidFile('/tmp/server.pid')
         with DaemonContext(pidfile=pid_file):
+            click.echo('Daemon starting...')
             return main()
     else:
             return main()
